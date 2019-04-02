f51a70edb8181b95bf4d96cf6d813c5b6596bdfe
Sara Kodeiri <skaordaeiri@outlook.com>
2019-04-02 16:11:24 +0430

    A3 Q1 complete

--- A3/A3/Q1MinCost.cs
+++ A3/A3/Q1MinCost.cs
@@ -13,45 +13,32 @@ namespace A3
 
         public override string Process(string inStr) =>
             TestTools.Process(inStr, (Func<long, long[][], long,long,long>)Solve);
-
-        //public class Node {
-
-        //    long distanceFromStart;
-        //    long index;
-
-        //    private Node (long Dist, long Index)
-        //    {
-        //        this.distanceFromStart = Dist;
-        //        this.index = Index;
-        //    }
-        //}
-
-
+        
         public long Solve(long nodeCount,long [][] edges, long startNode, long endNode)
         {
-            //Write Your Code Here
-            Graph graph = new Graph(nodeCount, edges, true);
-            var adjacencyList = graph.adjacencyList;
-
             long[] dist = new long[nodeCount];
+            long[] distCop = new long[nodeCount];
+            List<long> priorityQueue = new List<long>(); //keys are distances
 
             for (int i=0; i<dist.Length; i++)
+            {
                 dist[i] = int.MaxValue;
-            dist[startNode - 1] = 0;
-
-
-            //int[] priorityQueue = new int[nodeCount];
-            List<long> priorityQueue = new List<long>(); //keys are distances
-            for (int i = 0; i < dist.Length; i++)
+                distCop[i] = int.MaxValue;
                 priorityQueue.Add(dist[i]);
+            }
+            dist[startNode - 1] = 0;
+            distCop[startNode - 1] = 0;
+            priorityQueue[0] = 0;
+            
 
-            while(priorityQueue.Count() != 0)
+            while (priorityQueue.Count() != 0)
             {
                 //Extract Min
                 var u = priorityQueue.Min(); //u is min distance from start node
-                var index = Array.IndexOf(dist, u); //index is index of node with distance of u
+                var index = Array.IndexOf(distCop, u); //index is index of node with distance of u
                 priorityQueue.Remove(u);
-                
+                distCop[index] = -1;
+
                 for (int i=0; i < edges.Length; i++)
                 {
                     
@@ -63,6 +50,8 @@ namespace A3
                         if (dist[v] > dist[index] + edgeCost)
                         {
                             dist[v] = dist[index] + edgeCost;
+                            distCop[v] = dist[index] + edgeCost;
+                            
                             //change priority
                             for (int j=0; j < priorityQueue.Count(); j++)
 
@@ -71,17 +60,13 @@ namespace A3
                                     priorityQueue[j] = dist[v];
                                     break;
                                 }
-                                    
-                            
                         }
                     }
                 }
-                //var u = priorityQueue[0];
-                //var index = Array.IndexOf(priorityQueue, u);
-                //priorityQueue[index] = null;
+                
             }
 
-            return dist[endNode - 1];
+            return dist[endNode - 1] == int.MaxValue ? -1 : dist[endNode - 1];
         }
     }
 }
