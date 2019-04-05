03e87072c7794379e2b428fd5836ccc9fe0bb0fd
Sara Kodeiri <skaordaeiri@outlook.com>
2019-04-05 21:38:07 +0430

    Q2 complete.

--- A3/A3/Q2DetectingAnomalies.cs
+++ A3/A3/Q2DetectingAnomalies.cs
@@ -16,7 +16,33 @@ namespace A3
 
         public long Solve(long nodeCount, long[][] edges)
         {
-            //Write Your Code Here
+            long[] dist = new long[nodeCount + 1];
+            for (int i = 0; i < dist.Length; i++)
+                dist[i] = int.MaxValue;
+                
+            dist[0] = -1;
+            dist[1] = 0;
+
+            for (int i=0; i < nodeCount; i++)
+                for (int j=0; j < edges.Length; j++)
+                {
+                    long start = edges[j][0];
+                    long end = edges[j][1];
+                    long weight = edges[j][2];
+                    if (dist[end] > dist[start] + weight)
+                        dist[end] = dist[start] + weight;
+
+                }
+
+            for (int i = 0; i < edges.Length; i++)
+            {
+                long start = edges[i][0];
+                long end = edges[i][1];
+                long weight = edges[i][2];
+                if (dist[end] > dist[start] + weight)
+                    return 1;
+            }
+
             return 0;
         }
     }
--- A3/A3Tests/GradedTests.cs
+++ A3/A3Tests/GradedTests.cs
@@ -18,7 +18,7 @@ namespace A3.Tests
         {
             Processor[] problems = new Processor[] {
                new Q1MinCost("TD1"),
-               //new Q2DetectingAnomalies("TD2"),
+               new Q2DetectingAnomalies("TD2"),
                //new Q3ExchangingMoney("TD3"),
                //new Q4FriendSuggestion("TD4")
             };
