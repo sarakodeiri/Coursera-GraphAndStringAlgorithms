acdbaea3cbdc7a142900f1a55f5c702f0f38e0b4
Sara Kodeiri <skaordaeiri@gmail.com>
2019-04-21 17:26:40 +0430

    Q1 done.

--- A7/A7/Q1FindAllOccur.cs
+++ A7/A7/Q1FindAllOccur.cs
@@ -19,12 +19,35 @@ namespace A7
 
         public long[] Solve(string text, string pattern)
         {
-            long[] none = { -1 };
-            if (text.Length < pattern.Length)
-                return none;
-            else
-                return new long[0];
+            string patternDollarText = pattern + "$" + text;
+            long[] prefixFuncRes = ComputeprefixFunction(patternDollarText);
+            List<long> result = new List<long>();
+            int n = pattern.Length;
+            for (int i = n; i < patternDollarText.Length; i++)
+                if (prefixFuncRes[i] == n)
+                    result.Add(i - 2 * n);
 
+            long[] none = { -1 };
+
+            return result.Count() == 0 ? none : result.ToArray();
+        }
+
+        private long[] ComputeprefixFunction(string str)
+        {
+            long[] result = new long[str.Length];
+            result[0] = 0;
+            int border = 0;
+            for (int i = 1; i<str.Length; i++)
+            {
+                while (border > 0 && str[i] != str[border])
+                    border = (int)result[border - 1];
+                if (str[i] == str[border])
+                    border++;
+                else
+                    border = 0;
+                result[i] = border;
+            }
+            return result;
         }
     }
 }
